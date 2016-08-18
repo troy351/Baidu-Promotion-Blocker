6b49cedf9093fbdc2f8e9df00c2515d07e56ff29
troy <914053923@qq.com>
2016-08-18 17:56:55 +0800

    bug fixes

--- plugin/background.js
+++ plugin/background.js
@@ -3,7 +3,7 @@ function checkForValidUrl(tabId, changeInfo, tab) {
         return;
     }
     // for github.io from http to https
-    if (tab.url.match(/http:\/\/.+\.github\.io.*/) !== null) {
+    if (tab.url.match(/http:\/\/[a-zA-z0-9_]+\.github\.io\/.*/) !== null) {
         var url='https' + tab.url.substr(4);
         chrome.tabs.update(tabId, {url: url}, function () {
             console.log('github.io pages redirect from http to https');
